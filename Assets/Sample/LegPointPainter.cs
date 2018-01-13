ca8bec83c6b10ede99496ec7fe6151690ebe8c49
EsProgram <radwimps.september@gmail.com>
2018-01-13 19:46:59 +0900

    first

---
+++ Assets/Sample/LegPointPainter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Es.InkPainter;
+
+public class LegPointPainter : MonoBehaviour
+{
+	public Brush brush;
+	public InkCanvas canvas;
+
+	public int paintTiming = 3;
+	private int count;
+
+	// Use this for initialization
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		count++;
+		if (count >= paintTiming)
+		{
+			count = 0;
+			foreach (var p in collision.contacts)
+			{
+				var canvas = p.otherCollider.GetComponent<InkCanvas>();
+				if (canvas != null)
+					canvas.Paint(brush, p.point);
+			}
+		}
+	}
+}
---
+++ Assets/Sample/Reflect/Reflect.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Reflect : MonoBehaviour
+{
+	[SerializeField]
+	private Camera reflectCamera;
+
+	private new Renderer renderer;
+	private Material sharedMaterial;
+
+	private readonly int ShaderPropertyReflectTex = Shader.PropertyToID("_RefTex");
+
+	private void Start()
+	{
+		//反射用カメラにスクリーンと同サイズのバッファを設定し、反射用テクスチャとしてマテリアルにセットする
+		renderer = GetComponent<Renderer>();
+		sharedMaterial = renderer.sharedMaterial;
+		reflectCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 16);
+		sharedMaterial.SetTexture(ShaderPropertyReflectTex, reflectCamera.targetTexture);
+	}
+
+	private void OnWillRenderObject()
+	{
+		var cam = Camera.current;
+		if (cam == reflectCamera)
+		{
+			//反射用カメラの変換行列をマテリアルにセット
+			var refVMatrix = cam.worldToCameraMatrix;
+			var refPMatrix = GL.GetGPUProjectionMatrix(cam.projectionMatrix, false);
+			var refVP = refPMatrix * refVMatrix;
+			var refW = renderer.localToWorldMatrix;
+			sharedMaterial.SetMatrix("_RefVP", refVP);
+			sharedMaterial.SetMatrix("_RefW", refW);
+
+			if (Screen.width != reflectCamera.targetTexture.width || Screen.height != reflectCamera.targetTexture.height)
+			{
+				reflectCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 16);
+				sharedMaterial.SetTexture(ShaderPropertyReflectTex, reflectCamera.targetTexture);
+			}
+
+			if (!Application.isPlaying && sharedMaterial.GetTexture(ShaderPropertyReflectTex) == null)
+			{
+				sharedMaterial.SetTexture(ShaderPropertyReflectTex, reflectCamera.targetTexture);
+			}
+		}
+	}
+}
